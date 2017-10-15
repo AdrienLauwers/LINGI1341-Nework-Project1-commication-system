0806c21a7140f15f9fedd7e8649a3951bec9aacd
Donaschmi <donatien@schmitz.cc>
2017-10-15 20:37:49 +0200

    ajout dossier projet

---
+++ Projet1/receiver.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <sys/time.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h> /* * sockaddr_in6 */
+#include <netdb.h>
+#include "packet_interface.h"
+
+int
+main (int argc, char **argv)
+{
+
+  char *fichier = NULL;
+  char *hostname = NULL;
+  int port;
+  int c;
+  int index;
+  opterr = 0;
+
+  while ((c = getopt (argc, argv, "f:")) != -1)
+    switch (c)
+      {
+      case 'f':
+        fichier = optarg;
+        break;
+      case '?':
+        if (optopt == 'c')
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+      }
+
+  index = optind;
+  if(index+2 != argc){
+    fprintf(stderr, "Arguments invalides\n");
+    return 1;
+  }
+  hostname = argv[index];
+  port = atoi(argv[index+1]);
+
+  // TODO receive_data(hostname, port, fichier);
+  return EXIT_SUCCESS;
+}
