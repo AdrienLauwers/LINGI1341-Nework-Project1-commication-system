b513b6b0d489dd0e49c7886335d3abf765d6b034
Donaschmi <donatien@schmitz.cc>
2017-10-21 22:11:45 +0200

    ajout spécification

--- Projet1/client.h
+++ Projet1/client.h
@@ -1,8 +1,55 @@
 #ifndef __CLIENT_H_
 #define __CLIENT_H_
 
-
+/*
+ * Envoie de données lues dans un fichier ou dans le flux stdin vers un destinataire
+ * La fonction :
+ * - Crée un socket en IPV6 , UDP et se connecte à l'adresse de destination
+ * - Envoie des données par packet et réagit suivant s'il recoit un acquittement
+ *   un non-acquittement
+ *
+ * @hostname: L'addresse à laquelle on se connecte
+ * @port: Le port auquel on se connecte
+ * @file: Un nom de fichier contenant les données ou NULL si non spécifié
+ * @post: Toutes les données du contenues dans le fichier ou sur le stdin on été envoyée
+ *
+ * @return:
+ */
 void send_data(char *hostname, int port, char* file);
+
+/*
+ * Adapte le buffer en cas d'acquittement.
+ * La fonction verifie que:
+ *  -
+ *
+ * @small_seq: Pointeur vers le seqnum du plus petit element du buffer
+ * @seq: Numéro de la séquence acquittée
+ * @small_index: L'index du plus petit élément dans le buffer
+ * @window: La taille de la fenêtre
+ * @timestamp: Le temps auquel le packet a été envoyé par le sender,
+ *             ce temps est récupéré sur l'acquittement.
+ * @tv: Le timeout actuel
+ * @nbre_tv: Le nombre de calcul de rtt moyen déja réalisé
+ */
 void adapt_ack(int *small_seq,int seq, int *small_index,int window, uint32_t timestamp, struct timeval tv, int  nbre_tv);
+
+/*
+ * Mets à jours le timeout
+ * La fonction verifie que:
+ * - Le CRC32 du header recu est le mÃªme que celui decode a la fin
+ *   du header (en considerant le champ TR a 0)
+ * - S'il est present, le CRC32 du payload recu est le meme que celui
+ *   decode a la fin du payload
+ * - Le type du paquet est valide
+ * - La longueur du paquet et le champ TR sont valides et coherents
+ *   avec le nombre d'octets recus.
+ *
+ * @tv1: Le temps auquel on a recu l'acquittement
+ * @timestamp: Le temps auquel le packet a été envoyé
+ * @tv: Temps Timeout courant
+ * @nbre_tv: Nombre de calculé de adapt_rtt déja réalisé
+ *
+ * @post: tv contient le nouveau timeout recalculé
+ */
 void adapt_rtt(struct timeval tv1, uint32_t timestamp, struct timeval *tv, int nbre_tv);
 #endif
--- Projet1/server.h
+++ Projet1/server.h
@@ -1,8 +1,51 @@
 #ifndef __SERVER_H_
 #define __SERVER_H_
 
+/*
+ * Recois les données venant du sender et envoit un aqcquittement
+ * un non-acquittement en fonction du paquet recu
+ * La fonction :
+ * - Recois des packets, les décodes et envoie un (non-)acquittement
+ * - Ecris le contenu des packets soit dans un fichier soit sur le flux stdout
+ *
+ * @hostname: L'addresse à laquelle on se connecte
+ * @port: Le port auquel on se connecte
+ * @file: Un nom de fichier contenant les données ou NULL si non spécifié
+ *
+ * @post: Toutes les données recues sont écrites dans le fichier ou sur le stdout
+ */
 void receive_data(char* hostname, int port, char* file);
 
+/*
+ * Renvoit au sender un acquittement ou un non-acquittement en fonction du packet recu
+ * La fonction :
+ * - Encode un nouveau packet, l'envoie au sender
+ *
+ * @pkt_ack: Le paquet qui sera encodé et envoyé
+ * @seqnum: Le seqnum du packet que l'on a recu
+ * @sfd: Le file descriptor du socket utilisé
+ * @ack: PTYPE_ACK si acquittement, PTYPE_NACK si non-acquittement
+ * @time_data: Le timestamp du packet recu, à encoder dans le packet d'acquittement
+ * @window: La taille de la window
+ *
+ * @return: Renvoit 0 en cas de succès, sinon -1
+ */
 int send_ack(pkt_t *pkt_ack, int seqnum, int sfd, int ack, uint32_t time_data, int window);
+
+/*
+ * Mets à jours le buffer
+ * La fonction :
+ * - 
+ *
+ * @index:
+ * @seq_rcv: Le seqnum recu
+ * @buffer_payload:
+ * @buffer_len:
+ * @pkt_rcv: Le packet recu
+ * @window: La taille de la window
+ *
+ * @post: Renvoit 0 en cas de succès, sinon -1
+ */
+
 void add_buffer(int index, int seq_rcv, int seq_exp,char ** buffer_payload, int *buffer_len, pkt_t * pkt_rcv, int window);
 #endif
