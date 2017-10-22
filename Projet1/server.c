55c258d243ffee4c57480c53b3c8e3b019026b01
AdrienLauwers <adrien.lauwers@student.uclouvain.be>
2017-10-22 23:35:51 +0200

    correction bug

--- Projet1/server.c
+++ Projet1/server.c
@@ -95,9 +95,7 @@ void receive_data(char* hostname, int port, char* file){
 
 		//Cas ou on a reçu un packet
 		if(FD_ISSET(sfd, &read_set )) {
-			printf("JE RECOIIIS");
-			//printf("\nSleep\n");
-			sleep(1);
+
 			//on lit le packet encodée recu
      		 int length = read(sfd,(void *)packet_encoded, 1024);
 			//Si taille == 0 , réception du packet qui confirme la fin de transmission
@@ -109,7 +107,6 @@ void receive_data(char* hostname, int port, char* file){
 			}
 			//Si taille < 0 => problème
 			else if(length < 0){
-				//On envoie le dernier packet recu ?
 				pkt_del(pkt_rcv);
 				pkt_del(pkt_ack);
 				return;
@@ -172,7 +169,7 @@ void receive_data(char* hostname, int port, char* file){
 
 
 	close(sfd);
-	//close(fd) ??
+	close(fd);
 	pkt_del(pkt_ack);
   	pkt_del(pkt_rcv);
 }
@@ -240,7 +237,6 @@ int send_ack(pkt_t *pkt_ack, int seqnum, int sfd, int ack, uint32_t time_data, i
   size_t buf_len = 20;
   //Encodage du ACK/NACK et remplissage de la variable buf
   return_status = pkt_encode(pkt_ack, buf, &buf_len);
-	printf("\nlength of ack = %zu", buf_len);
   if(return_status != PKT_OK){
     perror("Encodage de l'acknowledge");
     return -1;
